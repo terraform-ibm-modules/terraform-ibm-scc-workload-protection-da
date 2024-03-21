1ca04a3a11da36b4957fe10e8633201de6243fc9
ocofaigh <ocofaigh@ie.ibm.com>
2024-03-21 13:32:18 +0000

    follow up issues

--- solutions/instances/README.md
+++ solutions/instances/README.md
@@ -7,3 +7,6 @@ This solution supports the following:
 - Provisioning and configuring of a Security and Compliance Center Workload Protection instance.
 
 **NB:** This solution is not intended to be called by one or more other modules since it contains a provider configurations, meaning it is not compatible with the `for_each`, `count`, and `depends_on` arguments. For more information see [Providers Within Modules](https://developer.hashicorp.com/terraform/language/modules/develop/providers)
+
+## Current limitation
+Currently this solution does not support attaching the Workload Protection instance to the SCC instance. That enhancement is being tracked in https://github.com/terraform-ibm-modules/terraform-ibm-scc-da/issues/23
--- solutions/instances/outputs.tf
+++ solutions/instances/outputs.tf
@@ -1,3 +1,76 @@
 ########################################################################################################################
 # Outputs
 ########################################################################################################################
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = module.resource_group.resource_group_name
+}
+
+output "resource_group_id" {
+  description = "Resource group ID"
+  value       = module.resource_group.resource_group_id
+}
+
+output "scc_cos_kms_key_crn" {
+  description = "SCC COS KMS Key CRN"
+  # if passing an existing bucket, then no KMS key is in play here, so output will be null
+  value = var.existing_scc_cos_bucket_name != null ? null : local.scc_cos_kms_key_crn
+}
+
+output "scc_cos_bucket_name" {
+  description = "SCC COS bucket name"
+  value       = local.cos_bucket_name
+}
+
+output "scc_id" {
+  description = "SCC instance ID"
+  value       = module.scc.id
+}
+
+output "scc_guid" {
+  description = "SCC instance guid"
+  value       = module.scc.guid
+}
+
+output "scc_crn" {
+  description = "SCC instance CRN"
+  value       = module.scc.crn
+}
+
+output "scc_name" {
+  description = "SCC instance name"
+  value       = module.scc.name
+}
+
+output "scc_wp_id" {
+  description = "SCC Workload Protection instance ID"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].id : null
+}
+
+output "scc_wp_crn" {
+  description = "SCC Workload Protection instance CRN"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].crn : null
+}
+
+output "scc_wp_name" {
+  description = "SCC Workload Protection instance name"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].name : null
+}
+
+output "scc_wp_ingestion_endpoint" {
+  description = "SCC Workload Protection instance ingestion endpoint"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].name : null
+}
+
+output "scc_wp_api_endpoint" {
+  description = "SCC Workload Protection API endpoint"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].api_endpoint : null
+  sensitive   = true
+}
+
+output "scc_wp_access_key" {
+  description = "SCC Workload Protection access key"
+  value       = var.provision_scc_workload_protection ? module.scc_wp[0].access_key : null
+  sensitive   = true
+}
