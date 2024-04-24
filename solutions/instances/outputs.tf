42896aa1e997aeaa4e7ad84e931c777d3d27750c
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-04-24 12:39:02 +0100

    Update outputs.tf

--- solutions/instances/outputs.tf
+++ solutions/instances/outputs.tf
@@ -20,7 +20,7 @@ output "scc_cos_kms_key_crn" {
 
 output "scc_cos_bucket_name" {
   description = "SCC COS bucket name"
-  value       = var.existing_scc_cos_bucket_name != null ? var.existing_scc_cos_bucket_name : module.cos.buckets[local.scc_cos_bucket_name].bucket_name
+  value       = var.existing_scc_cos_bucket_name != null ? var.existing_scc_cos_bucket_name : module.cos[0].buckets[local.scc_cos_bucket_name].bucket_name
 }
 
 output "scc_id" {
