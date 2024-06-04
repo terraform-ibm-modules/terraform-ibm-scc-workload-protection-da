76f4b0b9ad6b3dab4f87db2ad4a0bd372bc03c48
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-04 15:18:47 +0100

    chore(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v1.4.3 (#104)
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -46,7 +46,7 @@ module "cloud_monitoring" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.4.2"
+  version           = "1.4.3"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
