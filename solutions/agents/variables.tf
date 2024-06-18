204c0c9d59bff61b5fb09cb4980b0a01513081ad
Allen Dean <allen.dean@us.ibm.com>
2024-06-18 13:34:55 -0400

    Update descriptions
    - Differentiate features.titles and descriptions in catalog json.
    - Standardize on approved short name for Workload Protection. Get rid of SCC abbreviations
    - Also update readme file note.

--- solutions/agents/variables.tf
+++ solutions/agents/variables.tf
@@ -4,7 +4,7 @@
 
 variable "ibmcloud_api_key" {
   type        = string
-  description = "The API Key to use for IBM Cloud."
+  description = "The IBM Cloud API key to deploy resources."
   sensitive   = true
 }
 
@@ -14,35 +14,35 @@ variable "ibmcloud_api_key" {
 
 variable "name" {
   type        = string
-  description = "Helm release name."
+  description = "The Helm release name."
   default     = "ibm-scc-wp-agent"
 }
 
 variable "namespace" {
   type        = string
-  description = "Namespace of the Security and Compliance Workload Protection agent."
+  description = "The namespace of the Workload Protection agent."
   default     = "ibm-scc-wp"
 }
 
 variable "cluster_name" {
   type        = string
-  description = "Cluster name to add Security and Compliance Workload Protection agent to."
+  description = "The cluster name to add the Workload Protection agent to."
 }
 
 variable "access_key" {
   type        = string
-  description = "Security and Compliance Workload Protection instance access key."
+  description = "The Workload Protection instance access key."
   sensitive   = true
 }
 
 variable "region" {
   type        = string
-  description = "Region where Security and Compliance Workload Protection instance is created."
+  description = "The region where the Workload Protection instance is created."
 }
 
 variable "endpoint_type" {
   type        = string
-  description = "Specify the endpoint (public or private) for the Security and Compliance Center Workload Protection service."
+  description = "The endpoint for the Workload Protection service. Possible values: `public`, `private.`"
   default     = "private"
   validation {
     error_message = "The specified endpoint_type can be private or public only."
@@ -52,38 +52,38 @@ variable "endpoint_type" {
 
 variable "deployment_tag" {
   type        = string
-  description = "Sets a global tag that will be included in the components. It represents the mechanism from where the components have been installed (terraform, local...)."
+  description = "A global tag that is included in the components. The tag represents the mechanism where the components are installed. For example, `terraform` or `local`."
   default     = "terraform"
 }
 
 variable "kspm_deploy" {
   type        = bool
-  description = "Deploy Security and Compliance Workload Protection KSPM component."
+  description = "Whether to deploy the Workload Protection Kubernetes Security Posture Management component."
   default     = true
 }
 
 variable "node_analyzer_deploy" {
   type        = bool
-  description = "Deploy Security and Compliance Workload Protection node analyzer component."
+  description = "Whether to deploy the Workload Protection node analyzer component."
   default     = true
 }
 
 variable "host_scanner_deploy" {
   type        = bool
-  description = "Deploy Security and Compliance Workload Protection host scanner component. If node_analyzer_deploy false, this component will not be deployed."
+  description = "Whether to deploy the Workload Protection host scanner component. Applies only if `node_analyzer_deploy` is true."
   default     = true
 }
 
 variable "cluster_scanner_deploy" {
   type        = bool
-  description = "Deploy Security and Compliance Workload Protection cluster scanner component."
+  description = "Whether to deploy the Workload Protection cluster scanner component."
   default     = true
 }
 
 
 variable "cluster_endpoint_type" {
   type        = string
-  description = "Specify the endpoint (public or private) for the cluster."
+  description = "The endpoint for the cluster. Possible values: `public`, `private.`"
   default     = "private"
   validation {
     error_message = "The specified cluster_endpoint_type can be private or public only."
