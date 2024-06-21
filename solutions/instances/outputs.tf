c2700c6140ac370e7e628dbe42451fc1964d1efb
Aashiq-J <122446118+Aashiq-J@users.noreply.github.com>
2024-06-22 00:32:51 +0530

    feat: add scc cos related outputs (#126)

--- solutions/instances/outputs.tf
+++ solutions/instances/outputs.tf
@@ -12,17 +12,6 @@ output "resource_group_id" {
   value       = module.resource_group.resource_group_id
 }
 
-output "scc_cos_kms_key_crn" {
-  description = "SCC COS KMS Key CRN"
-  # if passing an existing bucket, then no KMS key is in play here, so output will be null
-  value = var.existing_scc_cos_bucket_name != null ? null : local.scc_cos_kms_key_crn
-}
-
-output "scc_cos_bucket_name" {
-  description = "SCC COS bucket name"
-  value       = var.existing_scc_cos_bucket_name != null ? var.existing_scc_cos_bucket_name : module.cos[0].buckets[local.scc_cos_bucket_name].bucket_name
-}
-
 output "scc_id" {
   description = "SCC instance ID"
   value       = module.scc.id
@@ -72,5 +61,51 @@ output "scc_workload_protection_api_endpoint" {
 output "scc_workload_protection_access_key" {
   description = "SCC Workload Protection access key"
   value       = var.provision_scc_workload_protection ? module.scc_wp[0].access_key : null
+  sensitive   = true
+}
+
+########################################################################################################################
+# SCC COS
+########################################################################################################################
+
+output "scc_cos_kms_key_crn" {
+  description = "SCC COS KMS Key CRN"
+  # if passing an existing bucket, then no KMS key is in play here, so output will be null
+  value = var.existing_scc_cos_bucket_name != null ? null : local.scc_cos_kms_key_crn
+}
+
+output "scc_cos_bucket_name" {
+  description = "SCC COS bucket name"
+  value       = var.existing_scc_cos_bucket_name != null ? var.existing_scc_cos_bucket_name : module.cos[0].buckets[local.scc_cos_bucket_name].bucket_name
+}
+
+output "scc_cos_bucket_config" {
+  description = "List of buckets created"
+  value       = var.existing_scc_cos_bucket_name != null ? null : module.cos[0].buckets[local.scc_cos_bucket_name]
+}
+
+output "scc_cos_instance_id" {
+  description = "SCC COS instance id"
+  value       = var.existing_scc_cos_bucket_name != null ? null : module.cos[0].cos_instance_id
+}
+
+output "scc_cos_instance_guid" {
+  description = "SCC COS instance guid"
+  value       = var.existing_scc_cos_bucket_name != null ? null : module.cos[0].cos_instance_guid
+}
+
+output "scc_cos_instance_name" {
+  description = "SCC COS instance name"
+  value       = var.existing_scc_cos_bucket_name != null ? null : local.cos_instance_name
+}
+
+output "scc_cos_instance_crn" {
+  description = "SCC COS instance crn"
+  value       = var.existing_scc_cos_bucket_name != null ? null : module.cos[0].cos_instance_crn
+}
+
+output "scc_cos_resource_keys" {
+  description = "List of resource keys"
+  value       = var.existing_scc_cos_bucket_name != null ? null : module.cos[0].resource_keys
   sensitive   = true
 }
