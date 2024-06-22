6c5be4e78a6306e28a34fc13cd8719bedd6473bd
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-22 16:09:12 -0400

    chore(deps): update terraform-module (#127)

--- tests/resources/existing-resources/agents/main.tf
+++ tests/resources/existing-resources/agents/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 
 module "landing_zone" {
-  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v5.24.6"
+  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks//module?ref=v5.24.7"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
