c9b59d9b70eaa28a7f93842d9395cf253e1d3d08
Carlos Tolon <carlos.tolon@sysdig.com>
2024-07-17 12:35:05 +0200

    fix: add CA certificate to provider for IKS integration

--- solutions/agents/provider.tf
+++ solutions/agents/provider.tf
@@ -10,6 +10,7 @@ provider "ibm" {
 provider "kubernetes" {
   host  = data.ibm_container_cluster_config.cluster_config.host
   token = data.ibm_container_cluster_config.cluster_config.token
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 provider "helm" {
