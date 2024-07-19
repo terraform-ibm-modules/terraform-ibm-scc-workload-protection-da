a0d182e3e61b3b6fe21f1a5f87d33a4df7733121
Carlos Tolon <ctolon22@users.noreply.github.com>
2024-07-19 14:05:18 +0200

    fix: added value for `cluster_ca_certificate` in provider config of the agents DA flavor to ensure it works in vanilla kubernetes (#151)

--- solutions/agents/provider.tf
+++ solutions/agents/provider.tf
@@ -14,8 +14,9 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host  = data.ibm_container_cluster_config.cluster_config.host
-    token = data.ibm_container_cluster_config.cluster_config.token
+    host                   = data.ibm_container_cluster_config.cluster_config.host
+    token                  = data.ibm_container_cluster_config.cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 }
 
