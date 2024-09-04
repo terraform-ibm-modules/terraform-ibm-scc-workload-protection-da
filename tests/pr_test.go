7ff40150fb8713f5cc9ba726ab09622a551e7524
ocofaigh <ocofaigh@ie.ibm.com>
2024-09-04 15:07:07 +0100

    test: remove workarounds and rebase

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -181,8 +181,6 @@ func TestRunUpgradeInstances(t *testing.T) {
 		"existing_kms_instance_crn":           permanentResources["hpcs_south_crn"],
 		"kms_endpoint_type":                   "public",
 		"management_endpoint_type_for_bucket": "public",
-		// Temp workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5586
-		"profile_attachments": []string{"SOC 2"},
 	}
 
 	output, err := options.RunTestUpgrade()
@@ -251,8 +249,6 @@ func TestRunExistingResourcesInstances(t *testing.T) {
 				"existing_cos_instance_crn":           terraform.Output(t, existingTerraformOptions, "cos_crn"),
 				"management_endpoint_type_for_bucket": "public",
 				"existing_en_crn":                     terraform.Output(t, existingTerraformOptions, "en_crn"),
-				// Temp workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5586
-				"profile_attachments": []string{"SOC 2"},
 			},
 		})
 
@@ -279,8 +275,6 @@ func TestRunExistingResourcesInstances(t *testing.T) {
 				"kms_endpoint_type":                   "public",
 				"existing_cos_instance_crn":           terraform.Output(t, existingTerraformOptions, "cos_crn"),
 				"management_endpoint_type_for_bucket": "public",
-				// Temp workaround for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5586
-				"profile_attachments": []string{"SOC 2"},
 			},
 		})
 
