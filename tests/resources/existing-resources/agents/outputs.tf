0d019ccd1f7e215194e2dc125521d586c0492c73
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-09-29 23:09:39 +0100

    fix: remove terraform version constraints from the agents DA variation (#200)

--- tests/resources/existing-resources/agents/outputs.tf
+++ tests/resources/existing-resources/agents/outputs.tf
@@ -10,16 +10,16 @@ output "cluster_data" {
 }
 
 output "workload_cluster_id" {
-  value       = lookup([for cluster in module.landing_zone.cluster_data : cluster if strcontains(cluster.resource_group_name, "workload")][0], "id", "")
+  value       = module.landing_zone.workload_cluster_id
   description = "ID of the workload cluster."
 }
 
 output "workload_cluster_name" {
-  value       = [for cluster_name in module.landing_zone.cluster_names : cluster_name if strcontains(cluster_name, "workload")][0]
+  value       = module.landing_zone.workload_cluster_name
   description = "Name of the workload cluster."
 }
 
 output "cluster_resource_group_id" {
-  value       = lookup([for cluster in module.landing_zone.cluster_data : cluster if strcontains(cluster.resource_group_name, "workload")][0], "resource_group_id", "")
+  value       = module.landing_zone.cluster_data["${var.prefix}-workload-cluster"].resource_group_id
   description = "Resource group ID of the workload cluster."
 }
