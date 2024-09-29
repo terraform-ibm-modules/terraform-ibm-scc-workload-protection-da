4dabf4ff5db92e11dfabcfcfcee8e010e4c946b6
ocofaigh <ocofaigh@ie.ibm.com>
2024-09-29 21:00:29 +0100

    fix test

--- tests/resources/existing-resources/agents/main.tf
+++ tests/resources/existing-resources/agents/main.tf
@@ -21,6 +21,9 @@ module "landing_zone" {
   tags                   = var.resource_tags
   add_atracker_route     = false
   enable_transit_gateway = false
+  # GHA runtime has no access to private
+  verify_cluster_network_readiness    = false
+  use_ibm_cloud_private_api_endpoints = false
 }
 
 ##############################################################################
