3f8ab8371a9da48688d8190dcf49a017364c9601
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-11-23 08:59:36 +0000

    chore(deps): update terraform-module

--- tests/resources/existing-resources/agents/main.tf
+++ tests/resources/existing-resources/agents/main.tf
@@ -32,7 +32,7 @@ module "landing_zone" {
 
 module "scc_wp_instance" {
   source            = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version           = "1.4.0"
+  version           = "1.4.2"
   name              = "${var.prefix}-scc-wp-instance"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
