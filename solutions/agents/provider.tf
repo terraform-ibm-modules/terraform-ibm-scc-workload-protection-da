933898fd554dc7c0e814135aeb59b8e7c8e61418
ocofaigh <ocofaigh@ie.ibm.com>
2024-11-25 18:07:59 +0000

    ask for cluster ID instead of name

--- solutions/agents/provider.tf
+++ solutions/agents/provider.tf
@@ -22,20 +22,21 @@ provider "helm" {
 
 data "ibm_container_vpc_cluster" "cluster" {
   count             = var.is_vpc_cluster ? 1 : 0
-  name              = var.cluster_name
+  name              = var.cluster_id
   wait_till         = var.wait_till
   wait_till_timeout = var.wait_till_timeout
 }
 
 data "ibm_container_cluster" "cluster" {
   count             = var.is_vpc_cluster ? 0 : 1
-  name              = var.cluster_name
+  name              = var.cluster_id
   wait_till         = var.wait_till
   wait_till_timeout = var.wait_till_timeout
 }
 
 data "ibm_container_cluster_config" "cluster_config" {
-  cluster_name_id = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].name : data.ibm_container_cluster.cluster[0].name
-  config_dir      = "${path.module}/kubeconfig"
-  endpoint_type   = var.cluster_endpoint_type
+  cluster_name_id   = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].id : data.ibm_container_cluster.cluster[0].id
+  config_dir        = "${path.module}/kubeconfig"
+  endpoint_type     = var.cluster_endpoint_type
+  resource_group_id = var.cluster_resource_group_id
 }
