cf3447cf2ca12dedd2f76a11631c9de0cdf74046
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-11-27 17:59:25 +0000

    feat: A fix was added to the `instances` variation to fix a breaking change that occurred on the SCC service backend related to Event Notification integration. It is now required to pass a value for `en_source_name` when passing a value for `existing_en_crn`. Optionally you can now also pass a value for `en_source_description`.<br>* The `cluster_name` input in the `agents` variation has been replaced by `cluster_id` and `cluster_resource_group_id`. (#233)

--- solutions/agents/provider.tf
+++ solutions/agents/provider.tf
@@ -8,8 +8,9 @@ provider "ibm" {
 }
 
 provider "kubernetes" {
-  host  = data.ibm_container_cluster_config.cluster_config.host
-  token = data.ibm_container_cluster_config.cluster_config.token
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  token                  = data.ibm_container_cluster_config.cluster_config.token
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 provider "helm" {
@@ -22,20 +23,21 @@ provider "helm" {
 
 data "ibm_container_vpc_cluster" "cluster" {
   count             = var.is_vpc_cluster ? 1 : 0
-  name              = var.cluster_name
+  name              = var.cluster_id
   wait_till         = var.wait_till
   wait_till_timeout = var.wait_till_timeout
 }
 
 data "ibm_container_cluster" "cluster" {
   count             = var.is_vpc_cluster ? 0 : 1
-  name              = var.cluster_name
+  name              = var.cluster_id
   wait_till         = var.wait_till
   wait_till_timeout = var.wait_till_timeout
 }
 
 data "ibm_container_cluster_config" "cluster_config" {
-  cluster_name_id = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].name : data.ibm_container_cluster.cluster[0].name
-  config_dir      = "${path.module}/kubeconfig"
-  endpoint_type   = var.cluster_endpoint_type
+  cluster_name_id   = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].id : data.ibm_container_cluster.cluster[0].id
+  config_dir        = "${path.module}/kubeconfig"
+  endpoint_type     = var.cluster_endpoint_type
+  resource_group_id = var.cluster_resource_group_id
 }
