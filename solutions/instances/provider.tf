37744b71fafd0a08b09dab66248962acb22fc4cb
Aditya Ranjan <48973656+Aditya-ranjan-16@users.noreply.github.com>
2024-11-28 16:21:13 +0530

    feat: exposed the ability to set the `provider_visibility` in the DA. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/IBM/latest/docs#visibility-1). (#229)

--- solutions/instances/provider.tf
+++ solutions/instances/provider.tf
@@ -5,16 +5,19 @@
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = local.scc_instance_region
+  visibility       = var.provider_visibility
 }
 
 provider "ibm" {
   alias            = "kms"
   ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
   region           = local.kms_region
+  visibility       = var.provider_visibility
 }
 
 provider "ibm" {
   alias            = "cos"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.cos_region
+  visibility       = var.provider_visibility
 }
