126f810a7446d09fab24ee412e576a985e5c9ef7
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-01-12 06:52:07 +0000

    fix(deps): update terraform-module

--- tests/resources/existing-resources/instances/main.tf
+++ tests/resources/existing-resources/instances/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.15.1"
+  version                = "8.16.5"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -32,7 +32,7 @@ module "cos" {
 
 module "cloud_monitoring" {
   source                  = "terraform-ibm-modules/observability-instances/ibm//modules/cloud_monitoring"
-  version                 = "3.3.1"
+  version                 = "3.4.0"
   resource_group_id       = module.resource_group.resource_group_id
   region                  = var.region
   instance_name           = "${var.prefix}-mon"
@@ -46,7 +46,7 @@ module "cloud_monitoring" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.14.9"
+  version           = "1.15.14"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
